5bce7cae95ff5944d833bd9974fe02a129a86343
Khalid Salman-Yusuf <olaniyisal@gmail.com>
2022-02-02 13:11:37 +0100

    Features/ certain changes

--- js/highscore.js
+++ js/highscore.js
@@ -3,8 +3,6 @@ const highscoreList = document.querySelector(".score")
 const highScores = JSON.parse(localStorage.getItem("highScore")) || []
 
 
-highscoreList.innerHTML = highScores
-    .map(score => {
-        return `<li class="high-score">${score.name} - ${score.score}</li>`
-    })
-    .join("")
+highscoreList.innerHTML = highScores.map(score => {
+    return `<li class="high-score">${score.name} - ${score.score}</li>`
+}).join("")
